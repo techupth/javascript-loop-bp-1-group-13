5dcc8838c360962ccf371de4498a88c387ed8020
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-09-19 13:53:32 +0700

    fix(ex-1-10): send assignment by gai

--- ex-2.js
+++ ex-2.js
@@ -1,6 +1,12 @@
 // Exercise #2: Displaying Restaurant Name
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
-
+let i = 0;
 // Start coding here
+
+while ( i in restaurants) {
+    newRestaurants = "Restaurant Name:" + restaurants[i]
+    i++
+    console.log(newRestaurants)
+}
 
--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,11 @@ let numbers = [100, 20, 3, 1000];
 let minNumber = 0;
 
 // Start coding here
+
+for (let i = 0; i < numbers[i]; i++ ){
+    if(numbers[i] < minNumber || i === 0){
+        minNumber = numbers[i];
+    }
+}
+
+console.log(minNumber)
